1569690b6293ce4b09014f6e1b8b31c5b1dce13a
David Kalla <david.kalla@centrum.cz>
2017-03-24 14:14:59 +0100

    test

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
-<?php
-error_reporting('E_ALL ^ E_NOTICE');
-global $ISO_common,$pageClass,$title;
-?>
+  <?php
+  error_reporting('E_ALL ^ E_NOTICE');
+  global $ISO_common,$pageClass,$title;
+  ?>
 <?php
 error_reporting('E_ALL ^ E_NOTICE');
 ?>
